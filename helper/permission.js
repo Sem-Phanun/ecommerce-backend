8785159fd803acb41d59e540d90b9e2da46f3895
Sem-Phanun <phanunsem097@gmai.com>
2024-02-02 14:58:24 +0700

    refactor on permission file

--- helper/permission.js
+++ helper/permission.js
@@ -3,11 +3,11 @@ import database from '../config/db.js'
 export const getPermission = async (id) => {
     var sql = "SELECT " +
     " p.code" +
-    " FROM tbl_staff staff" +
-    " INNER JOIN tbl_role r ON staff.role_id = r.role_id" +
-    " INNER JOIN tbl_role_permission rp ON r.role_id = rp.role_id" +
-    " INNER JOIN tbl_permission p ON rp.permission_id = p.permission_id" +
-    " WHERE staff.staff_id = ?"
+    " FROM employee emp" +
+    " INNER JOIN role r ON emp.role_id = r.role_id" +
+    " INNER JOIN permission_role rp ON r.role_id = rp.role_id" +
+    " INNER JOIN permission p ON rp.permission_id = p.permission_id" +
+    " WHERE emp.employee_id = ?"
     var list = await database.query(sql,[id])
     var tmpArr = []
     list.forEach((item) => {
